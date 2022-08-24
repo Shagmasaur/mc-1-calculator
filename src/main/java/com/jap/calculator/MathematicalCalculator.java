efd02a83e38bf384ace599daafc9335f9b38e7c4
abhishek.lashley <abhishek.lashley@gmail.com>
2022-08-24 17:29:07 +0530

    mathematical Calculator done

--- src/main/java/com/jap/calculator/MathematicalCalculator.java
+++ src/main/java/com/jap/calculator/MathematicalCalculator.java
@@ -4,24 +4,33 @@ public class MathematicalCalculator {
 
 	public int add(int num1, int num2)
 	{	
-		return -1;
+		return num1+num2;
 	}
 	public int subtract(int num1, int num2)
 	{	
-		return -2;
+		return num1 - num2;
 	}
 	public int multiply(int num1, int num2)
 	{	
-		return -1;
+		return num1 * num2;
 	}
 	public int divide(int num1, int num2)
 	{
 		//write the code and handle the ArithmeticException
-         return -1;
-
+		try
+		{
+			int  result = num1 / num2;
+			return result;
+		}
+		catch(ArithmeticException e)
+		{
+			int result = 0;
+			return result;
+		}
 	}
+
 	public int modulo(int num1, int num2)
-	{	
-		return-1;
+	{
+		return num1 % num2;
 	}
 }
